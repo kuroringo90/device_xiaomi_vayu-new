22a31ac1e72a89ad536a714f9bdfab5e786ba169
kuroringo90 <ssss9999@gmx.it>
2022-05-21 22:33:50 +0200

    Kraken Bringup

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit some common PE stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common kraken stuff.
+$(call inherit-product, vendor/aosp/config/common_phone.mk)
 
 # Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -33,6 +33,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Device Settings
 $(call inherit-product-if-exists, vendor/xiaomi/devicesettings/config.mk)
 # Google Camera 
-$(call inherit-product-if-exists, vendor/Gcam/config.mk)
+# $(call inherit-product-if-exists, vendor/Gcam/config.mk)
 # Pixel Launcher
-$(call inherit-product-if-exists, vendor/pixel/launcher/config.mk)
+# $(call inherit-product-if-exists, vendor/pixel/launcher/config.mk)
