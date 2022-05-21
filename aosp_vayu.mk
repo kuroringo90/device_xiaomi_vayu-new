c7e87e1949414379a1659e0f430f4e7d2ff66882
kuroringo90 <ssss9999@gmx.it>
2022-05-21 22:34:54 +0200

    Briungup Fix

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit some common kraken stuff.
-$(call inherit-product, vendor/aosp/config/common_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
